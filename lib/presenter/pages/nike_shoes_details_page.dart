4eaff50d64a3a5ad85eb350de38c9afa611187d5
João Victor Reis Alves <joao.reisa13@gmail.com>
2021-12-01 14:00:15 -0300

    Adding floating action buttons on details page, and changing the hero label

--- lib/presenter/pages/nike_shoes_details_page.dart
+++ lib/presenter/pages/nike_shoes_details_page.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 class NikeShoesDetailsPage extends StatelessWidget {
-  const NikeShoesDetailsPage({ Key? key }) : super(key: key);
+  const NikeShoesDetailsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  FloatingActionButton(
+                    heroTag: 'fav_1',
+                    backgroundColor: Colors.white,
+                    child: const Icon(
+                      Icons.favorite,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {},
+                  ),
+                  const Spacer(),
+                  FloatingActionButton(
+                    heroTag: 'fav_2',
+                    backgroundColor: Colors.black,
+                    child: const Icon(Icons.shopping_cart),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
